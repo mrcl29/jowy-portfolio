8218e430a681f355a396c343af8a8f5c5074ebc6
mrcl29 <m.llobera@outlook.com>
2025-09-04 20:22:48 +0200

    Update multilanguage

---
+++ src/constants/i18n/en.ts
@@ -0,0 +1,25 @@
+// src/constants/i18n/en.ts
+import photo1 from "../../assets/photo/photo.png";
+
+export const sections = [
+  {
+    image: photo1,
+    title: "DJ Jowy",
+    color: "#065f46",
+    mask: "mask-fade-top-bottom-right",
+  },
+  { 
+    image: photo1, 
+    title: "Producer",
+    color: "#fde68a", 
+    mask: "mask-fade-all-sides" 
+  },
+  {
+    image: photo1,
+    title: "Sound Engineer",
+    color: "#22d3ee",
+    mask: "mask-fade-top-bottom-left",
+  },
+];
+
+export const description = "Jowy: DJ, producer and sound engineer. Explore my portfolio of music, track production and professional audio services."
---
+++ src/constants/i18n/es.ts
@@ -0,0 +1,25 @@
+// src/constants/i18n/es.ts
+import photo1 from "../../assets/photo/photo.png";
+
+export const sections = [
+  {
+    image: photo1,
+    title: "DJ Jowy",
+    color: "#065f46",
+    mask: "mask-fade-top-bottom-right",
+  },
+  { 
+    image: photo1, 
+    title: "Productor",
+    color: "#fde68a", 
+    mask: "mask-fade-all-sides" 
+  },
+  {
+    image: photo1,
+    title: "Técnico de Sonido",
+    color: "#22d3ee",
+    mask: "mask-fade-top-bottom-left",
+  },
+];
+
+export const description = "Jowy: DJ, productor y técnico de sonido. Explora mi portafolio de música, producción de tracks y servicios de audio profesional."
