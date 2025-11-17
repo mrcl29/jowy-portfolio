87165a11628089f0af7ec6870c958fbd2f693206
mrcl29 <m.llobera@outlook.com>
2025-11-17 23:58:47 +0000

    Modal + Edicion de estilos

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+// tailwind.config.mjs
+import flowbite from "flowbite/plugin";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [flowbite],
+};
